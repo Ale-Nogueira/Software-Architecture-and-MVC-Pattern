72c82c524e824fbe54dcf587f3b097ce7cc0645f
Alexandre Nogueira <alexandrenog2013@gmail.com>
2025-04-17 15:15:40 -0300

    feat: implementing project routes for pets and people

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -4,10 +4,11 @@ from src.models.sqlite.settings.connection import db_connection_handler
 
 # importar blueprints
 from src.main.routes.pets_routes import pets_route_bp
-
+from src.main.routes.person_routes import person_route_bp
 db_connection_handler.connect_to_db()
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(pets_route_bp)
+app.register_blueprint(person_route_bp)
